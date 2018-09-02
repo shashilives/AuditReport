b43284c4f7998be2ea9cb5c84de4b7d493fe88c7
Shashi Kumar <shashikumarlives@gmail.com>
2018-09-02 22:49:48 +0530

    autocomplete added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CdkTableModule } from '@angular/cdk/table';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { DatatableComponent } from './components/datatable/datatable.component';
 import { ChartsComponent } from './components/charts/charts.component';
-import { MatTableModule, MatSortModule } from '@angular/material';
+// import { MatTableModule, MatSortModule, MatAutocompleteModule } from '@angular/material';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DbsearchComponent } from './components/dbsearch/dbsearch.component';
+
+import {
+  MatAutocompleteModule,
+  MatInputModule,
+  MatSortModule,
+  MatTableModule,
+} from '@angular/material';
 
 @NgModule({
   exports: [
+    CdkTableModule,
+    MatAutocompleteModule,
     MatTableModule,
+    MatInputModule,
     MatSortModule
-  ]
+
+  ],
+  declarations: []
 })
 export class DemoMaterialModule { }
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     DatatableComponent,
-    ChartsComponent
-
+    ChartsComponent,
+    DbsearchComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     BrowserAnimationsModule,
-    ReactiveFormsModule,
-    DemoMaterialModule
+    FormsModule,
+    DemoMaterialModule,
+    ReactiveFormsModule
 
   ],
   providers: [],
